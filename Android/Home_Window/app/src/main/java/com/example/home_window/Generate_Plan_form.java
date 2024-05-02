9b3689baffb163bc7a104bde56733366831846dc
tikna123 <ankitalex010@gmail.com>
2024-05-02 23:28:09 +0530

    various additions

---
+++ Android/Home_Window/app/src/main/java/com/example/home_window/Generate_Plan_form.java
@@ -0,0 +1,14 @@
+package com.example.home_window;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Generate_Plan_form extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_generate_plan_form);
+    }
+}
